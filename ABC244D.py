618f572e8215020f58a95c8a6903d56a413b7b4c
hy <81627289+ip-hy1014@users.noreply.github.com>
2022-03-24 23:56:33 +0900

    Update ABC244D.py

--- ABC244D.py
+++ ABC244D.py
@@ -2,3 +2,13 @@ rgb = ["R G B","G B R", "B R G"]
 s = input()
 t = input()
 print("Yes" if (s in rgb) == (t in rgb) else "No")
+
+#初期状態で不一致の人数が2ならNo
+#別解
+s = input()
+t = input()
+ans = 0
+for i,j in zip(s,t):
+  if i!=j:
+    ans += 1
+print("Yes" if ans!=2 else "No")
